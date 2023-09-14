9d646012be40ef7f48560da9dfd67016284ea8d6
User1 <example@gmail.com>
2023-09-14 18:51:31 +0300

    Добавьте файлы проекта.

---
+++ Kuromi/Kuromi.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>	
+#include <string>
+
+using namespace std;
+
+struct Point {
+	string firstName;
+	string lastName;
+	int age;
+	int phoneNumber;
+};
+
+int main(Point) {
+	setlocale(LC_ALL, "");
+	Point user;
+	cout << "Введите Имя: " << endl;
+	cin >> user.firstName;
+	cout << "Введите Фамилию: " << endl;
+	cin >> user.lastName;
+	cout << "Введите ваш возраст: " << endl;
+	cin >> user.age;
+	cout << "Введите ваш номер телефона: " << endl;
+	cin >> user.phoneNumber;
+}
